64c0f7f1008dbc945f8e525dbfa6de3a2dd23119
Peng Lei <charleypeng@gmail.com>
2022-05-06 18:27:32 +0800

    fix merge bug

--- BlazorLeaflet/BlazorLeaflet/LeafletInterops.cs
+++ BlazorLeaflet/BlazorLeaflet/LeafletInterops.cs
@@ -87,21 +87,6 @@ namespace BlazorLeaflet
         public static ValueTask<float> GetZoom(IJSRuntime jsRuntime, string mapId) =>
             jsRuntime.InvokeAsync<float>($"{_BaseObjectContainer}.getZoom", mapId);
 
-        // Private class only for deserialization from JSON (since the JSON names on the bounds are "_southWest"
-        // with the _). Since deserialization in JSRuntime is non-customizable, this is a good solution for now.
-        private class _Bounds
-        {
-            public LatLng _southWest { get; set; }
-            public LatLng _northEast { get; set; }
-
-            public Bounds AsBounds() => new Bounds(_southWest, _northEast);
-        }
-
-        public static async Task<Bounds> GetBounds(IJSRuntime jsRuntime, string mapId)
-        {
-            return (await jsRuntime.InvokeAsync<_Bounds>($"{_BaseObjectContainer}.getBounds", mapId)).AsBounds();
-        }
-
         public static async Task SetZoom(IJSRuntime jsRuntime, string mapId, float zoomLevel)
         {
             await jsRuntime.InvokeVoidAsync($"{_BaseObjectContainer}.setZoom", mapId, zoomLevel);
